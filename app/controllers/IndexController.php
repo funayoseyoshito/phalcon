28cae22488e4b8358e5ed49e2784712ee2ebb30d
funayose yoshito <funayose4410@gmail.com>
2015-10-12 16:37:58 +0900

    プロジェクト基本構成dir追加

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+class IndexController extends \Phalcon\Mvc\Controller
+{
+    public function indexAction()
+    {
+        echo '<h1>hear is Index Controller</h1>';
+    }
+
+    public function testAction()
+    {
+        echo '<h1>Index test</h1>';
+    }
+}
---
+++ app/controllers/SignupController.php
@@ -0,0 +1,8 @@
+<?php
+class SignupController extends \Phalcon\Mvc\Controller
+{
+    public function indexAction()
+    {
+        echo 'SignupController';
+    }
+}
